1a1048fb22086ce44bb6cf3502311608d0a6d1e3
61524 <unconfigured@null.spigotmc.org>
2022-02-07 20:05:17 -0600

    Made JobField abstract

--- src/org/launchcode/techjobs_oo/Main.java
+++ src/org/launchcode/techjobs_oo/Main.java
@@ -9,10 +9,13 @@ public class Main {
         Job job2 = new Job("Web Developer", new Employer("LaunchCode"), new Location("St. Louis"), new PositionType("Front-end developer"), new CoreCompetency("JavaScript"));
         Job job3 = new Job("Ice cream taster", new Employer(""), new Location("Home"), new PositionType("UX"), new CoreCompetency("Taste"));
 
+        Job job4 = new Job("testjob", new Employer(""), new Location("Home"), new PositionType("UX"), new CoreCompetency("Taste"));
+
         ArrayList<Job> jobs = new ArrayList<>();
         jobs.add(job1);
         jobs.add(job2);
         jobs.add(job3);
+        jobs.add(job4);
 
         for (Job job : jobs){
             System.out.println(job);
